23005b605112eca097f96f7a3f47219205e78157
Neil Nelanuthala <neilnelan95@gmail.com>
2021-10-05 13:06:14 -0400

    Added a comment

--- Bus_Simulation/src/com/busSimulation/BusStop.java
+++ Bus_Simulation/src/com/busSimulation/BusStop.java
@@ -14,6 +14,8 @@ public class BusStop {
 	static int passengerArriveFreqLow;
 	static int passengerArriveFreqHigh;
 	
+	//Test Comment
+	
 	
 
 	public BusStop(int id, int frequencyLow, int frequencyHigh, int totalStops) {
